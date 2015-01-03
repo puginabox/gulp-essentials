9b80bf1f10220964152994b692d63eab06ccab57
Griffin Byron <puginabox@gmail.com>
2015-01-03 10:49:24 -0500

    adding gulp setup

---
+++ gulpfile_manifest.js
@@ -0,0 +1,109 @@
+/*  Manifest
+*   
+*   - &Create variable references
+*   - &Autoload gulp plugins
+*   - &Load common gulp utilities
+*   - &Create comments for minified files
+*   - &Lint the code
+*   - &Minify and bundle the JavaScript
+*   - &CSS Preprocessing
+*   - &Minify and bundle the CSS
+*   - &Bundle the JS, CSS, and compress images
+*   - &Compress images
+*   - &Run builds for staging/production: switch. Run `gulp --production`
+*   - &Copy left over files to builds and show a toast.
+*   - &Clean - remove all files from the output folder
+*   - &Watch file and re-run the linter
+
+
+to search for section: &<section>
+*/
+//---------------------------------------------------------------------------------------|
+
+/*
+ * &Create variable references
+ */
+
+
+
+
+/*
+ * &Autoload gulp plugins
+ */
+
+
+
+/*
+ * &Load common gulp utilities
+ */
+
+
+
+/*
+ * &Create comments for minified files
+ */
+
+
+
+
+
+/*
+ * &Lint the code
+ */
+
+
+
+
+/*
+ * &Minify and bundle the JavaScript
+ */
+
+
+
+
+/*
+ * &CSS Preprocessing
+ */
+
+
+
+
+
+/*
+ * Minify and bundle the CSS
+ */
+
+
+
+
+/*
+ * Compress images
+ */
+
+
+
+
+
+/*
+ * &Run builds for staging/production: switch. Run `gulp --production`
+ */
+
+        
+        
+        
+/*
+ * &Copy left over files to builds and show a toast.
+ */
+
+        
+
+        
+/*
+*  &Copy left over files to builds and show a toast.
+*/
+
+
+/*
+ * &Watch file and re-run the linter
+ */
+
